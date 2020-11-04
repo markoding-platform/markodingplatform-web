44fd9b2495265962153ca78e6f01d0e7e303754b
Ibnu Imam <ibnuimam@IT-KHD-MacBook-Pro.local>
2020-11-04 19:29:22 +0700

    base project

--- utils/getCookie.js
+++ utils/getCookie.js
@@ -1,5 +1,11 @@
 import cookie from 'cookie';
 
+/**
+ *
+ * @param req Object of request data
+ * @param labelOfCookie String of cookie name/label
+ */
+
 export default function getCookie(req, labelOfCookie) {
 	const cookies = cookie.parse(
 		req && req.headers ? req.headers.cookie || '' : document.cookie
