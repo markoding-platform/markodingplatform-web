bdcb7bbf4f363c4aaa1849e6b5e5a7319994c43b
Ibnu Imam <ibnuimam@IT-KHD-MacBook-Pro.local>
2020-11-04 19:15:01 +0700

    initial project

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/custom.boostrap.scss'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,5 @@
 
 export default (req, res) => {
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json({ name: 'Markoding Platform v.0.1.0' })
 }
