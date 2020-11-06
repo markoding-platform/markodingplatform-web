b0871c674c1b999410d47f893b57c1f85b9e324a
azhariemuhammad <azharie_muhammad@live.com>
2020-11-06 23:36:13 +0800

    [ADD] nav children

--- components/NavMenu/index.js
+++ components/NavMenu/index.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { BsChevronDown } from 'react-icons/bs';
 import navItems from './navItems';
+import { rotateDown, rotate } from './styles.module.scss';
 
 const NavMenu = () => {
+	const [isShowNavTree, setShowNavTree] = useState(false);
+	const handleShowNavTree = () => {
+		setShowNavTree((prevState) => !prevState);
+	};
 	return (
   <ul className="list-group list-group-flush bg-primary">
     {navItems.map((navItem) => (
-      <Link key={navItem.id} href={navItem.link}>
-        <li className="list-group-item bg-primary justify-content-between align-items-center">
-          <a href={navItem.link}>
+      <li key={navItem.id} className="list-group-item bg-primary">
+        {navItem?.children ? (
+          <div
+            className="d-flex justify-content-between"
+            onClick={handleShowNavTree}
+          >
             {navItem.text}
-            {navItem.withBadge && (
-            <span className="badge badge-danger text-right badge-pill">14</span>
-							)}
-          </a>
-        </li>
-      </Link>
+            <BsChevronDown
+              className={`${rotate} ${isShowNavTree && rotateDown}`}
+              size={20}
+            />
+          </div>
+					) : (
+  <Link href={navItem.link}>
+    <a href={navItem.link}>
+      <div className="d-flex">
+        {navItem.text}
+        {navItem.withBadge && (
+        <span className="ml-2 badge badge-danger text-right badge-pill">
+          14
+        </span>
+									)}
+      </div>
+    </a>
+  </Link>
+					)}
+
+        {navItem?.children && isShowNavTree && (
+        <ul className="list-group list-group-flush bg-primary">
+          {navItem.children.map((item) => (
+            <li
+              key={item.id}
+              className="list-group-item border-0 bg-primary justify-content-between align-items-center"
+            >
+              <Link href={item.link}>
+                <a href={item.link}>{item.text}</a>
+              </Link>
+            </li>
+							))}
+        </ul>
+					)}
+      </li>
 			))}
   </ul>
 	);
