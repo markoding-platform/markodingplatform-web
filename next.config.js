f87a42b4da1d5fcd14c5735f4d3339cde2bc41dc
Ibnu Imam <ibnuimam@IT-KHD-MacBook-Pro.local>
2020-11-17 18:04:11 +0700

    try amplify cors

--- next.config.js
+++ next.config.js
@@ -3,24 +3,26 @@ const withImages = require('next-images');
 const dotEnvResult = require('dotenv').config();
 
 if (dotEnvResult.error) {
-	throw dotEnvResult.error;
+  throw dotEnvResult.error;
 }
 
-module.exports = withPWA(withImages({
-	pwa: {
-		disable: process.env.NODE_ENV !== 'production',
-		dest: 'public'
-	},
-	images: {
-		domains: ['image.freepik.com'], // next/image for external link freepick just for dummy
-		dest: 'public',
-	},
-	inlineImageLimit: false, // Base4/Data URL encoding is not supported when using the next/image
-	fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'svg'],
-	env: {
-		TITLE: process.env.TITLE
-	},
-	webpack(config) {
-		return config;
-	},
-}));
+module.exports = withPWA(
+  withImages({
+    pwa: {
+      disable: process.env.NODE_ENV !== 'production',
+      dest: 'public',
+    },
+    images: {
+      domains: ['image.freepik.com'], // next/image for external link freepick just for dummy
+      dest: 'public',
+    },
+    inlineImageLimit: false,
+    fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'svg'],
+    env: {
+      TITLE: process.env.TITLE,
+    },
+    webpack(config) {
+      return config;
+    },
+  })
+);
