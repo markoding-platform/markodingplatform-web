56faf5ee979ed9c2a28cf8c370a64b0acb70666e
Ibnu Imam <ibnuimam@IT-KHD-MacBook-Pro.local>
2020-12-03 10:43:30 +0700

    [add] sso verify success page

---
+++ pages/sso/verify/success.js
@@ -0,0 +1,15 @@
+const SsoVerifySuccess = () => {
+  /*
+  TODO: logut user here and show information for relogin
+  why relogin? for get new info user is true verifyed or not.
+  */
+
+  return (
+    <div className="text-center">
+      <p>Email kamu berhasil di verifikasi</p>
+      <p>Silahkan login kembali dengan akun kamu</p>
+    </div>
+  );
+};
+
+export default SsoVerifySuccess;
