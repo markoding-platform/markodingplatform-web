fa43484965389315452dbeda83f0dc95bd93d5e5
azhariemuhammad <azharie_muhammad@live.com>
2020-12-07 21:05:20 +0800

    [ADD] styling mobile footer

--- components/NavMenu/index.js
+++ components/NavMenu/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { BsChevronDown } from 'react-icons/bs';
+
+import Footer from 'components/Footer';
 import navItems from './navItems';
 import { rotateDown, rotate, bgPrimary } from './styles.module.scss';
 
@@ -12,55 +14,60 @@ const NavMenu = () => {
     );
   };
   return (
-    <ul className={`list-group list-group-flush ${bgPrimary}`}>
-      {navItems.map((navItem) => (
-        <li key={navItem.id} className={`list-group-item ${bgPrimary}`}>
-          {navItem?.children ? (
-            <div
-              role="presentation"
-              className="d-flex justify-content-between"
-              onClick={() => handleShowNavTree(navItem.id)}
-            >
-              {navItem.text}
-              <BsChevronDown
-                className={`${rotate} ${
-                  showNavTreeId === navItem.id && rotateDown
-                }`}
-                size={20}
-              />
-            </div>
-          ) : (
-            <Link href={navItem.link}>
-              <a href={navItem.link}>
-                <div className="d-flex align-items-start">
-                  {navItem.text}
-                  {navItem.withBadge && (
-                    <span className="ml-2 badge badge-danger text-right badge-pill">
-                      14
-                    </span>
-                  )}
-                </div>
-              </a>
-            </Link>
-          )}
+    <>
+      <ul className={`list-group list-group-flush ${bgPrimary}`}>
+        {navItems.map((navItem) => (
+          <li key={navItem.id} className={`list-group-item ${bgPrimary}`}>
+            {navItem?.children ? (
+              <div
+                role="presentation"
+                className="d-flex justify-content-between"
+                onClick={() => handleShowNavTree(navItem.id)}
+              >
+                {navItem.text}
+                <BsChevronDown
+                  className={`${rotate} ${
+                    showNavTreeId === navItem.id && rotateDown
+                  }`}
+                  size={20}
+                />
+              </div>
+            ) : (
+              <Link href={navItem.link}>
+                <a href={navItem.link}>
+                  <div className="d-flex align-items-start">
+                    {navItem.text}
+                    {navItem.withBadge && (
+                      <span className="ml-2 badge badge-danger text-right badge-pill">
+                        14
+                      </span>
+                    )}
+                  </div>
+                </a>
+              </Link>
+            )}
 
-          {navItem?.children && showNavTreeId === navItem.id && (
-            <ul className="list-group list-group-flush">
-              {navItem.children.map((item) => (
-                <li
-                  key={item.id}
-                  className={`${bgPrimary} list-group-item border-0 justify-content-between align-items-center`}
-                >
-                  <Link href={item.link}>
-                    <a href={item.link}>{item.text}</a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        </li>
-      ))}
-    </ul>
+            {navItem?.children && showNavTreeId === navItem.id && (
+              <ul className="list-group list-group-flush">
+                {navItem.children.map((item) => (
+                  <li
+                    key={item.id}
+                    className={`${bgPrimary} list-group-item border-0 justify-content-between align-items-center`}
+                  >
+                    <Link href={item.link}>
+                      <a href={item.link}>{item.text}</a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div className="d-lg-none">
+        <Footer />
+      </div>
+    </>
   );
 };
 
