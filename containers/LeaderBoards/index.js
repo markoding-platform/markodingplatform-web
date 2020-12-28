4f4c3be1218ec908498fcfd515522f3b7d91de3a
azhariemuhammad <azharie_muhammad@live.com>
2020-12-28 22:01:35 +0800

    change file name

--- containers/LeaderBoards/index.js
+++ containers/LeaderBoards/index.js
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
 const DynamicLeaderboardsContainer = dynamic(() =>
-  import(/* webpackChunkName: "leaderboards-container" */ './View')
+  import(/* webpackChunkName: "leaderboards-container" */ './Leaderboards')
 );
 
 export default DynamicLeaderboardsContainer;
--- pages/leaderboards.js
+++ pages/leaderboards.js
@@ -1,6 +1,6 @@
 import Layout from 'components/Layout';
 import PointBadgeWrapper from 'components/PointBadgeWrapper';
-import LeaderboardsContainer from 'containers/Leaderboards';
+import DynamicLeaderboardsContainer from 'containers/Leaderboards';
 
 const Leaderboards = () => {
   return (
@@ -10,7 +10,7 @@ const Leaderboards = () => {
           <PointBadgeWrapper />
         </div>
         <div className="inner-section pb-5">
-          <LeaderboardsContainer />
+          <DynamicLeaderboardsContainer />
         </div>
       </div>
     </Layout>
