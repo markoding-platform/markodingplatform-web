9fd9fd79c6b9c8b1bef408104da24b50a1bb6a88
azhariemuhammad <azharie_muhammad@live.com>
2020-12-28 18:57:27 +0800

    fixing course data

--- containers/Home/CourseWidget/CourseWidget.js
+++ containers/Home/CourseWidget/CourseWidget.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import useSWR from 'swr';
 import { arrayOf, string, shape } from 'prop-types';
 
+import SkilvulFetch from 'libraries/SkilvulFetch';
 import SectionCardWrapper from 'components/SectionCardWrapper';
 import CourseCard from 'components/CourseCard';
+import WidgetLoader from '../WidgetLoader';
+import courseMap from '../../../map/courseMap';
 
-const CourseWidget = ({ courses }) => {
+const CourseWidget = () => {
+  let courses = [];
+  const { data: courseRes, error } = useSWR(
+    '/api/course?limit=6&offset=1',
+    SkilvulFetch
+  );
+  const isLoading = !courseRes && !error;
+  if (courseRes && courseRes.products) {
+    courses = courseRes.products.map(courseMap);
+  }
+
   return (
     <>
+      {isLoading && <WidgetLoader />}
       <div className="pb-5">
         <SectionCardWrapper title="Kelas Online" link="/course">
-          {courses.map((course) => (
-            <CourseCard
-              key={course.id}
-              imageUrl={course.imageUrl}
-              title={course.title}
-              description={course.description}
-              link={course.link}
-            />
-          ))}
+          {courses &&
+            courses.map((course) => (
+              <CourseCard
+                key={course.id}
+                imageUrl={course.imageUrl}
+                title={course.title}
+                description={course.description}
+                link={course.link}
+              />
+            ))}
         </SectionCardWrapper>
       </div>
     </>
