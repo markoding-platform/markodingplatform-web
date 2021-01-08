99f1f7d7d3bd2b42288fb1b9e4020894ae337c07
azhariemuhammad <azharie_muhammad@live.com>
2021-01-09 00:36:42 +0800

    change logo

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,8 @@ import { BiPhone } from 'react-icons/bi';
 import { FiMail } from 'react-icons/fi';
 
 import Icon from 'components/Icons';
-import MarkodingLogo from 'components/MarkodingLogo';
+import skilvulLogo from 'public/assets/skilvul-logo-with-text.png';
+import dicLogo from 'public/assets/dic-logo.png';
 import icFacebook from 'svgs/ic-facebook.svg';
 import icYoutube from 'svgs/ic-youtube.svg';
 import icInstagram from 'svgs/ic-instagram.svg';
@@ -14,7 +15,7 @@ import {
   footerItem,
   footerChild,
   leftSide,
-  textBold,
+  stySkilvul,
   contacts,
   footerInner,
   footerInfo,
@@ -84,7 +85,18 @@ const Footer = () => (
   <section className={footerContainer}>
     <div className={footerInner}>
       <div className={leftSide}>
-        <MarkodingLogo />
+        <div className="pt-2">
+          <Link href="/">
+            <a>
+              <img
+                src={dicLogo}
+                width="406px"
+                height="24px"
+                alt="digital innovation challenge logo"
+              />
+            </a>
+          </Link>
+        </div>
         <h6 className={markodingMessage}>
           Kami memberdayakan remaja yang kurang beruntung dengan mengajarkan
           coding kepada siswa sekolah menengah atas di daerah marjinal di
@@ -120,8 +132,12 @@ const Footer = () => (
     <hr />
     <div className="d-flex justify-content-between flex-wrap px-4 mb-5">
       <p className="text-secondary">
-        {/* eslint-disable react/jsx-one-expression-per-line */}©
-        <span className={textBold}>2020 MARKODING</span>. All Rights Reserved
+        <span className="pr-2">Learning Platform by</span>
+        <Link href="/">
+          <a>
+            <img src={skilvulLogo} className={stySkilvul} alt="skilvul logo" />
+          </a>
+        </Link>
       </p>
       <div className={contacts}>
         <p className="text-secondary">
