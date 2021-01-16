2c900c35def01ce3effd76bd96603ebd11d6e7c1
azhariemuhammad <azharie_muhammad@live.com>
2021-01-16 17:35:07 +0800

    implement api idea on dashboard

--- containers/IdeaAndSolutionContainer/SnippetIdea.js
+++ containers/IdeaAndSolutionContainer/SnippetIdea.js
@@ -1,19 +1,21 @@
-import { number } from 'prop-types';
+import { number, string } from 'prop-types';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Button from 'react-bootstrap/Button';
 import { BsFillHeartFill } from 'react-icons/bs';
 import { IoMdChatbubbles } from 'react-icons/io';
 
-import useIdeaSolution from 'hooks/useIdeaSolution';
 import { ideaImage } from './style.module.scss';
 
-const SnippetIdea = ({ likeCount, commentCount, ideaId }) => {
+const SnippetIdea = ({
+  likeCount,
+  commentCount,
+  imageIdea,
+  ideaId,
+  solutionVision,
+  solutionName,
+}) => {
   const { push } = useRouter();
-  const { data } = useIdeaSolution({ url: `/ideas/${ideaId}` });
-  const idea = data?.result || {};
-
-  const imageIdea = idea.solutionSupportingPhotos?.[0] || '';
 
   const handleClickViewIdea = () => {
     push(`/idea/${ideaId}`);
@@ -24,35 +26,22 @@ const SnippetIdea = ({ likeCount, commentCount, ideaId }) => {
   };
   return (
     <div>
-      <div className="py-2">
+      <div className="py-2 position-relative">
         {imageIdea && (
           <Image
             src={imageIdea}
-            alt={idea.solutionName}
+            alt={solutionName}
             width={500}
-            height={300}
+            height={220}
             layout="responsive"
             className={ideaImage}
           />
         )}
       </div>
       <div className="pb-4">
-        <h4>{idea.solutionName}</h4>
+        <h4>{solutionName}</h4>
       </div>
-      {/* <div className="d-flex py-2">
-        <Image
-          src={Avatar}
-          alt="avatar-image"
-          width={52}
-          height={52}
-          className={ideaImage}
-        />
-        <div className="pl-2">
-          <h6 className="m-0 pb-2">Ariqah Hasanah</h6>
-          <p className="text-3rd m-0">11/11/2020</p>
-        </div>
-      </div> */}
-      <div dangerouslySetInnerHTML={{ __html: idea.solutionVision }} />
+      <div dangerouslySetInnerHTML={{ __html: solutionVision }} />
       <div className="d-flex justify-content-between mt-5">
         <div className="d-flex align-items-center justify-content-start">
           <div className="mr-4">
@@ -78,11 +67,19 @@ const SnippetIdea = ({ likeCount, commentCount, ideaId }) => {
 };
 
 SnippetIdea.defaultProps = {
+  imageIdea: '',
+  ideaId: '',
+  solutionVision: '',
+  solutionName: '',
   commentCount: 12,
   likeCount: 3,
 };
 
 SnippetIdea.propTypes = {
+  imageIdea: string,
+  ideaId: string,
+  solutionName: string,
+  solutionVision: string,
   commentCount: number,
   likeCount: number,
 };
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,12 @@
 import Layout from 'components/Layout';
 import PointBadgeWrapper from 'components/PointBadgeWrapper';
 import DashboardContainer from 'containers/Dashboard';
+import { homeContent } from 'styles/home.module.scss';
 
 export default function Dashboard() {
   return (
     <Layout activeMenu="/idea">
-      <div className="main-content">
+      <div className={homeContent}>
         <div className="pb-4">
           <PointBadgeWrapper />
         </div>
