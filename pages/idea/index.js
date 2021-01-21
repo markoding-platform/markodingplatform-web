e35785992ebc515238ae6ee9871d067a97c267fc
Muhammad Amri A. Azhary <azharie_muhammad@live.com>
2021-01-21 23:08:20 +0800

    add filter and sort button (#95)

--- pages/idea/index.js
+++ pages/idea/index.js
@@ -1,9 +1,14 @@
 import Layout from 'components/Layout';
+import { ErrorBoundary } from 'react-error-boundary';
 import PointBadgeWrapper from 'components/PointBadgeWrapper';
 import IdeaAndSolutionContainer from 'containers/IdeaAndSolutionContainer';
+import useErrorHandler from 'hooks/useErrorHandler';
+import ErrorFallback from 'components/ErrorFallback';
+
 import { homeContent } from 'styles/home.module.scss';
 
 export default function Idea() {
+  const { logError } = useErrorHandler();
   return (
     <Layout activeMenu="/idea">
       <div className={homeContent}>
@@ -11,7 +16,9 @@ export default function Idea() {
           <PointBadgeWrapper />
         </div>
         <div className="inner-section pb-5">
-          <IdeaAndSolutionContainer />
+          <ErrorBoundary FallbackComponent={ErrorFallback} onError={logError}>
+            <IdeaAndSolutionContainer />
+          </ErrorBoundary>
         </div>
       </div>
     </Layout>
