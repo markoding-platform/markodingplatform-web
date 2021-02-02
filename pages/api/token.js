350a10240705ea6722e3c43a61b1227a27a998ab
Ibnu Imam <me@ibnuimam.com>
2021-02-02 16:15:44 +0700

    [modify] temp token in file server api (#130)

--- pages/api/token.js
+++ pages/api/token.js
@@ -1,4 +1,6 @@
 import nextConnect from 'next-connect';
+import fs from 'fs';
+import moment from 'moment';
 
 const dotEnvResult = require('dotenv').config();
 
@@ -6,50 +8,105 @@ if (dotEnvResult.error) {
   throw dotEnvResult.error;
 }
 
+const getToken = async () => {
+  try {
+    const response = await fetch(`${process.env.SKILVUL_API_URL}/oauth/token`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        clientId: process.env.SSO_CLIENT_ID,
+        clientSecret: process.env.SSO_CLIENT_SECRET,
+      }),
+    });
+    return response.json();
+  } catch (error) {
+    return {
+      error: true,
+      message: error,
+    };
+  }
+};
+
+const getRefreshToken = async (refreshToken) => {
+  try {
+    const response = await fetch(
+      `${process.env.SKILVUL_API_URL}/oauth/token/refresh`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          clientId: process.env.SSO_CLIENT_ID,
+          refreshToken,
+        }),
+      }
+    );
+    return response.json();
+  } catch (error) {
+    return {
+      error: true,
+      message: error,
+    };
+  }
+};
+
+const writeFile = async (data) => {
+  // set token data
+  const stringifyData = JSON.stringify(data);
+  // write to file token
+  fs.writeFile('skilvul.token.json', stringifyData, () => {});
+  return true;
+};
+
 const tokenApi = nextConnect()
   .get(async (req, res) => {
-    try {
-      const response = await fetch(
-        `${process.env.SKILVUL_API_URL}/oauth/token`,
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            clientId: process.env.SSO_CLIENT_ID,
-            clientSecret: process.env.SSO_CLIENT_SECRET,
-          }),
+    const tokenFile = fs.readFileSync('skilvul.token.json', 'utf8');
+    if (tokenFile !== '') {
+      const obj = JSON.parse(tokenFile);
+      const now = moment(new Date());
+      const end = moment(obj.exp);
+      const duration = now.diff(end, 'hours');
+      if (duration > 12) {
+        // expired call refreshToken
+        const resTokenRef = getRefreshToken(obj.refreshToken);
+        if (
+          resTokenRef &&
+          resTokenRef.accessToken &&
+          resTokenRef.error === null
+        ) {
+          const newObj = obj;
+          newObj.accessToken = resTokenRef.accessToken;
+          writeFile(newObj);
+          res.status(200).json(newObj);
+        } else {
+          const resToken = await getToken();
+          if (resToken && !resToken.error) {
+            const data = {
+              accessToken: resToken.accessToken,
+              refreshToken: resToken.refreshToken,
+              exp: moment(new Date()),
+            };
+            writeFile(data);
+            res.status(200).json(data);
+          } else {
+            res.status(500).json(resToken);
+          }
         }
-      );
-      const data = await response.json();
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(500).json({
-        error: true,
-        message: error,
-      });
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      const { refreshToken } = await req.body;
-      const response = await fetch(
-        `${process.env.SKILVUL_API_URL}/oauth/token/refresh`,
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            clientId: process.env.SSO_CLIENT_ID,
-            refreshToken,
-          }),
-        }
-      );
-      const data = await response.json();
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(500).json({
-        error: true,
-        message: error,
-      });
+      } else {
+        res.status(200).json(obj);
+      }
+    } else {
+      const resToken = await getToken();
+      if (resToken && !resToken.error) {
+        const data = {
+          accessToken: resToken.accessToken,
+          refreshToken: resToken.refreshToken,
+          exp: moment(new Date()),
+        };
+        writeFile(data);
+        res.status(200).json(data);
+      } else {
+        res.status(500).json(resToken);
+      }
     }
   })
   .patch(async () => {
