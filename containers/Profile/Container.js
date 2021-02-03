4ce916d8b0e8e87fca003d131614ba3f98f535fe
azhariemuhammad <azharie_muhammad@live.com>
2021-02-03 12:09:33 +0800

    fix rerender

--- containers/Profile/Container.js
+++ containers/Profile/Container.js
@@ -1,7 +1,10 @@
+import { useCallback, useEffect, useState, useRef } from 'react';
 import { shape, string } from 'prop-types';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import SkilvulFetch from 'libraries/SkilvulFetch';
+import { locationSchoolMap, professionsMap } from 'map/dropdownMap';
 import AccountInfo from './AccountInfo';
 import AccountMenu from './AccountMenu';
 import CompanyInfo from './CompanyInfo';
@@ -20,6 +23,46 @@ const ProfileContainer = ({
 }) => {
   const { name, profile = {} } = user || {};
   const { profileType = {}, imageUrl, schoolName } = profile || {};
+  const [provinces, setProvinces] = useState([]);
+  const [professions, setProfessions] = useState([]);
+  const [cities, setCities] = useState([]);
+  const firstLoad = useRef(false);
+
+  const getProvinces = useCallback(async () => {
+    const provResult = await SkilvulFetch('/api/skilvul?path=/provinces');
+    if (provResult && provResult.provinces) {
+      setProvinces(provResult.provinces.map(locationSchoolMap));
+    }
+  }, []);
+
+  const getProfessions = useCallback(async () => {
+    const professionResult = await SkilvulFetch(
+      `/api/skilvul?path=/professions`
+    );
+    if (professionResult && professionResult.professions) {
+      setProfessions(professionResult.professions.map(professionsMap));
+    }
+  }, []);
+
+  const getCities = useCallback(async () => {
+    const currProvinceId = province?.id || '';
+    if (!currProvinceId) return;
+    const cityResult = await SkilvulFetch(
+      `/api/skilvul?path=/cities?provinceId=${currProvinceId}`
+    );
+    if (cityResult && cityResult.cities) {
+      setCities(cityResult.cities.map(locationSchoolMap));
+    }
+  }, [province?.id]);
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      getProvinces();
+      getProfessions();
+      getCities();
+      firstLoad.current = true;
+    }
+  }, [getCities, getProfessions, getProvinces]);
 
   return (
     <Row>
@@ -39,12 +82,20 @@ const ProfileContainer = ({
             lastName={lastName}
             birthDate={birthDate}
             profession={profession}
+            professions={professions}
             province={province}
             city={city}
             defaultgender={gender}
             email={email}
+            provinces={provinces}
+            cityList={cities}
           />
-          <CompanyInfo profileType={profileType} profile={profile} />
+          <CompanyInfo
+            profileType={profileType}
+            profile={profile}
+            provinces={provinces}
+            cityList={cities}
+          />
         </form>
       </Col>
     </Row>
