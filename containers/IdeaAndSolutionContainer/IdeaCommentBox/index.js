1aa306736dee6dab3737dbd0230fce0e25da8e40
azhariemuhammad <azharie_muhammad@live.com>
2021-02-14 16:25:16 +0800

    remove dead code

--- containers/IdeaAndSolutionContainer/IdeaCommentBox/index.js
+++ containers/IdeaAndSolutionContainer/IdeaCommentBox/index.js
@@ -1,6 +1,4 @@
 import dynamic from 'next/dynamic';
-// import Loading from 'components/Loading';
-// import React from 'react';
 
 const DynamicIdeaCommentBox = dynamic(() =>
   import(/* webpackChunkName: "idea-comments" */ './View')
