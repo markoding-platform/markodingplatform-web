c1f9d272142d0bee2f6b6d0b051e3a489053cfc6
azhariemuhammad <azharie_muhammad@live.com>
2021-03-03 21:19:45 +0800

    remove deadcode

--- containers/ProfileRouteContainer/index.js
+++ containers/ProfileRouteContainer/index.js
@@ -3,7 +3,8 @@ import Loading from 'components/Loading';
 import React from 'react';
 
 const DynamicProfileRouteContainer = dynamic(
-  () => import(/* webpackChunkName: "profile-container" */ './Container'),
+  () =>
+    import(/* webpackChunkName: "profile--route-container" */ './Container'),
   {
     loading: () => <Loading />,
   }
--- pages/dashboard/profile/index.js
+++ pages/dashboard/profile/index.js
@@ -1,19 +1,11 @@
 import DynamicProfileRouteContainer from 'containers/ProfileRouteContainer';
-import { useRouter } from 'next/router';
+
 import { shape } from 'prop-types';
 
 import withAuthSync from 'hoc/withAuthSync';
 
 const ProfileRoute = ({ user }) => {
-  const { pathname } = useRouter();
-  let isEditProfile = false;
-  if (pathname.includes('/settings')) {
-    isEditProfile = true;
-  }
-
-  return (
-    <DynamicProfileRouteContainer isEditProfile={isEditProfile} user={user} />
-  );
+  return <DynamicProfileRouteContainer user={user} />;
 };
 
 ProfileRoute.propTypes = {
