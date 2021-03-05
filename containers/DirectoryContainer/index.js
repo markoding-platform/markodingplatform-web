747ed4978b80ce32db5bfde5a55c7e71a341e6d1
Ibnu Imam <me@ibnuimam.com>
2021-03-05 23:19:48 +0700

    add pagination (#162)

--- containers/DirectoryContainer/index.js
+++ containers/DirectoryContainer/index.js
@@ -1,5 +1,5 @@
 import styles from 'styles/directory.module.scss';
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import range from 'utils/range';
 import BoxLoader from 'components/Shimmer/Box';
@@ -8,10 +8,19 @@ import Icon from 'components/Icons';
 import Col from 'react-bootstrap/Col';
 import UserCard from 'components/UserCard';
 import Row from 'react-bootstrap/Row';
-import warningBell from '../../svgs/warning-bell.svg';
+import { useRouter } from 'next/router';
+import { LIMIT_PER_PAGE } from 'containers/IdeaAndSolutionContainer/constant';
+import Pagination from 'components/Pagination';
 import userMap from '../../map/userMap';
+import warningBell from '../../svgs/warning-bell.svg';
 
 const DirectoryContainer = ({ directorySlug }) => {
+  const limit = 9;
+  const router = useRouter();
+  const { query } = router;
+  const currentOffset = Number(query?.start) || 0;
+  const currentPage = Number(query?.page) || 1;
+
   let userTypeSlug = 'students';
   if (directorySlug === 'teacher' || directorySlug === 'teachers') {
     userTypeSlug = 'teachers';
@@ -19,9 +28,20 @@ const DirectoryContainer = ({ directorySlug }) => {
     userTypeSlug = 'mentors';
   }
 
-  const { data, error } = useDirectory({ path: `/users/${userTypeSlug}` });
-  const result = data?.result ? data.result.map(userMap) : [];
-  const isLoading = !data && !error;
+  const { isLoading, data, pages } = useDirectory({
+    path: `/users/${userTypeSlug}?limit=${limit}&offset=${currentOffset}`,
+  });
+  const result = data.map(userMap) || [];
+
+  const handlePageChanged = useCallback(
+    (page) => {
+      const offset = LIMIT_PER_PAGE * page - LIMIT_PER_PAGE;
+      router.replace(
+        `/directory/${userTypeSlug}/?page=${page}&start=${offset}`
+      );
+    },
+    [router]
+  );
 
   const renderLoader = () => {
     const loaderArr = [];
@@ -39,20 +59,31 @@ const DirectoryContainer = ({ directorySlug }) => {
     <>
       {isLoading && renderLoader()}
       {!isLoading && result.length > 0 ? (
-        <Row>
-          {result.map((dir) => (
-            <Col key={dir.id} xs={6} lg={4}>
-              <div className={styles.directoryGrid}>
-                <UserCard
-                  imageUrl={dir.imageUrl}
-                  name={dir.name}
-                  description={dir.bio}
-                  link={`/user/${dir.id}`}
-                />
-              </div>
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {result.map((dir) => (
+              <Col key={dir.id} xs={6} lg={4}>
+                <div className={styles.directoryGrid}>
+                  <UserCard
+                    imageUrl={dir.imageUrl}
+                    name={dir.name}
+                    description={dir.bio}
+                    link={`/user/${dir.id}`}
+                  />
+                </div>
+              </Col>
+            ))}
+          </Row>
+          <div className="d-flex justify-content-center mt-5">
+            <Pagination
+              totalRecords={pages.count}
+              totalPages={pages.totalPages}
+              pageLimit={LIMIT_PER_PAGE}
+              onPageChanged={handlePageChanged}
+              defaultPage={currentPage}
+            />
+          </div>
+        </>
       ) : (
         <div className={styles.emptyState}>
           <Icon src={warningBell} size={170} className="mb-3" />
--- hooks/useDirectory.js
+++ hooks/useDirectory.js
@@ -1,8 +1,18 @@
 import useSWR from 'swr';
 import MarkodingFetch from 'libraries/MarkodingFetch';
 
-const useDirectory = ({ path }) => {
-  return useSWR(path, MarkodingFetch);
+const useDirectory = ({ url }) => {
+  const { data: response, error } = useSWR(url, MarkodingFetch);
+  const result = response?.result || {};
+  const { data, pages = {} } = result;
+  const dirs = data || [];
+  const isLoading = !response && !error;
+  return {
+    data: dirs,
+    pages,
+    error,
+    isLoading,
+  };
 };
 
 export default useDirectory;
