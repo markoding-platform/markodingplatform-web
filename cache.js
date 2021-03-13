49462d0376051a38835a11c92d19f27c818a1e8f
azhariemuhammad <azharie_muhammad@live.com>
2021-03-13 14:51:06 +0800

    update runtimeCaching start_url

--- cache.js
+++ cache.js
@@ -1,5 +1,20 @@
 module.exports = [
   {
+    // MUST be the same as "start_url" in manifest.json
+    urlPattern: '/',
+    // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
+    // use StaleWhileRevalidate if you want to prompt user to reload when new version available
+    handler: 'NetworkFirst',
+    options: {
+      // don't change cache name
+      cacheName: 'start-url',
+      expiration: {
+        maxEntries: 1,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      },
+    },
+  },
+  {
     urlPattern: /\/api\/.*$/i,
     handler: 'NetworkFirst',
     method: 'GET',
